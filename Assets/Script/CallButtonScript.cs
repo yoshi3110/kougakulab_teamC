9a55f82367ec0a77c09b9155a2a11246f43d852c
yoshi3110 <2001yoshi0215@gmail.com>
2020-11-30 19:34:31 +0900

    aaa

---
+++ Assets/Script/CallButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CallButtonScript : MonoBehaviour
+{
+    public Text buttonText;
+    [SerializeField] string sceneName;
+    // Start is called before the first frame update
+    public void setName(string scene)
+    {
+        sceneName = scene;
+        buttonText.text = sceneName;
+    }
+
+    public void addScene()
+    {
+        Application.LoadLevelAdditive(sceneName);
+    }
+}
---
+++ Assets/Script/MainSceneManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSceneManager : MonoBehaviour
+{
+    public InputField passField;
+    public subScene[] subScenes;
+    public GameObject callButtonObj;
+    public Transform scrollViewContent;
+    [System.Serializable]
+    public class subScene
+    {
+        public string sceneName;
+        public string password;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void sendPassword()
+    {
+        int i = checkPassWord(passField.text);
+        if (i >= 0)
+        {
+            GameObject newButton = Instantiate(callButtonObj);
+            newButton.transform.SetParent(scrollViewContent);
+            newButton.SetActive(true);
+            CallButtonScript callButtonScript = newButton.GetComponent<CallButtonScript>();
+            callButtonScript.setName(subScenes[i].sceneName);
+        }
+    }
+
+    int checkPassWord(string inputStr)
+    {
+        Debug.Log("check:" + inputStr);
+        for (int i = 0; i < subScenes.Length; i++)
+        {
+            if (inputStr.Equals(subScenes[i].password))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
