90fc426e110d2145f26172000275c82c71f20637
yoshi3110 <2001yoshi0215@gmail.com>
2020-12-11 13:27:39 +0900

    おおおお

---
+++ Assets/Script/VirtualWallSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualWallSwitch : MonoBehaviour
+{
+    public SerialHandler serialHandler;
+    // Start is called before the first frame update
+   
+    public void VWSwitch(int i)
+    {
+        serialHandler.Write(i.ToString() + "\n");
+        Debug.Log("sent:"+i);
+    }
+}
